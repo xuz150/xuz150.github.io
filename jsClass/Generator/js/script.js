1983fc5a576427d2db402eecf64d697e4669ac65
Zhiyang Xu <xuz150@newschool.edu>
2017-10-11 19:32:58 -0400

    Modification 2

--- jsClass/Generator/js/script.js
+++ jsClass/Generator/js/script.js
@@ -1,7 +1,7 @@
 function getRandomColor(){
-	return 'rgb(' + (Math.floor((256-199)*Math.random()) + 200) + ','
-                     + (Math.floor((256-199)*Math.random()) + 200) + ','
-                     + (Math.floor((256-199)*Math.random()) + 200) + ')';
+	return 'rgb(' + (Math.floor((256-199)*Math.random()) + 180) + ','
+                     + (Math.floor((256-199)*Math.random()) + 180) + ','
+                     + (Math.floor((256-199)*Math.random()) + 180) + ')';
 }
 
 $(document).ready(function() {
